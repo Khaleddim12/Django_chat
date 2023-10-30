37d63acb20c831d90878f3c508c2b2e37fd2bfd9
Khaled <khaleddim12@gmail.com>
2023-10-30 12:41:41 +0300

    first commit

---
+++ chat_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Room
+
+
+def index_view(request):
+    return render(request, 'index.html', {
+        'rooms': Room.objects.all(),
+    })
+
+
+def room_view(request, room_name):
+    chat_room, created = Room.objects.get_or_create(name=room_name)
+    return render(request, 'room.html', {
+        'room': chat_room,
+    })
